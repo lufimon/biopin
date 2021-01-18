847e5c8e95e2c0cc57bac6887547fc1ca353a484
LUFIMON <tanuphong.p@cdg.co.th>
2021-01-18 13:24:24 +0700

    update, main activity

--- app/src/main/java/th/co/cdgs/mobile/biomatric/MainActivity.kt
+++ app/src/main/java/th/co/cdgs/mobile/biomatric/MainActivity.kt
@@ -1,43 +1,52 @@
 package th.co.cdgs.mobile.biomatric
 
 import android.os.Bundle
-import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import th.co.cdgs.mobile.biopin.SecurityAuthentication
 import th.co.cdgs.mobile.biopin.pincode.security.PFLoginListener
 
 
-class MainActivity : AppCompatActivity(){
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         SecurityAuthentication.Builder(this)
-            .setPinLength(6)
-            .setContainerView(R.id.container_view)
-            .setLoginListenerr(object : PFLoginListener{
-                override fun onCodeInputSuccessful() {
-                    Log.e("onCodeInputSuccessful", "onCodeInputSuccessful")
-                }
+                .setPinLength(6)
+                .setContainerView(R.id.container_view)
+                .setUseFingerprint(false)
+                .setAutoFingerprint(false)
+                .setLoginListenerr(object : PFLoginListener {
+                    override fun onCodeInputSuccessful() {
+                        toast("onCodeInputSuccessful")
+                    }
 
-                override fun onFingerprintSuccessful() {
-                    Log.e("onFingerprintSuccessful", "onFingerprintSuccessful")
-                }
+                    override fun onFingerprintSuccessful() {
+                        toast("onFingerprintSuccessful")
+                    }
 
-                override fun onPinLoginFailed() {
-                }
+                    override fun onPinLoginFailed() {
+                        toast("onPinLoginFailed")
+                    }
 
-                override fun onFingerprintLoginFailed() {
-                }
+                    override fun onFingerprintLoginFailed() {
+                        toast("onFingerprintLoginFailed")
+                    }
 
-                override fun onFingerprintError(errorCode: Int, errorString: String) {
-                }
+                    override fun onFingerprintError(errorCode: Int, errorString: String) {
+                        toast("onFingerprintError$errorCode$errorString")
+                    }
 
-                override fun onForgetPassCode() {
-                    recreate()
-                }
-            })
-            .build()
-            .create()
+                    override fun onForgetPassCode() {
+                        toast("onForgetPassCode")
+                    }
+                })
+                .build()
+                .create()
+    }
+
+    private fun toast(msg: String) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
     }
 }
