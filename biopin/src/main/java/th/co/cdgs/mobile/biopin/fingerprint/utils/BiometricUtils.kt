a2e639b226eae52cb01c4f8f44535f3c7a1c33c4
LUFIMON <tanuphong.p@cdg.co.th>
2021-01-18 10:53:16 +0700

    update layout passcode

--- biopin/src/main/java/th/co/cdgs/mobile/biopin/fingerprint/utils/BiometricUtils.kt
+++ biopin/src/main/java/th/co/cdgs/mobile/biopin/fingerprint/utils/BiometricUtils.kt
@@ -1,9 +1,11 @@
 package th.co.cdgs.mobile.biopin.fingerprint.utils
 
+import android.Manifest
 import android.Manifest.permission.USE_FINGERPRINT
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
+import androidx.biometric.BiometricManager
 import androidx.core.app.ActivityCompat
 import androidx.core.hardware.fingerprint.FingerprintManagerCompat
 
@@ -19,19 +21,53 @@ object BiometricUtils {
     }
 
     fun isHardwareSupported(context: Context): Boolean {
-        val fingerprintManager = FingerprintManagerCompat.from(context)
-        return fingerprintManager.isHardwareDetected
+        return when {
+            isSdkVersionSupported() -> {
+                val fingerprintManager = FingerprintManagerCompat.from(context)
+                fingerprintManager.isHardwareDetected
+            }
+            isBiometricPromptEnabled() -> {
+                val packageManager: PackageManager = context.packageManager
+                packageManager.hasSystemFeature(PackageManager.FEATURE_FINGERPRINT)
+            }
+            else -> {
+                false
+            }
+        }
     }
 
     fun isFingerprintAvailable(context: Context): Boolean {
-        val fingerprintManager = FingerprintManagerCompat.from(context)
-        return fingerprintManager.hasEnrolledFingerprints()
+        return when {
+            isSdkVersionSupported() -> {
+                val fingerprintManager = FingerprintManagerCompat.from(context)
+                fingerprintManager.hasEnrolledFingerprints()
+            }
+            isBiometricPromptEnabled() -> {
+                BiometricManager.from(context).canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK) == BiometricManager.BIOMETRIC_SUCCESS
+            }
+            else -> {
+                false
+            }
+        }
     }
 
     fun isPermissionGranted(context: Context): Boolean {
-        return ActivityCompat.checkSelfPermission(
-            context,
-            USE_FINGERPRINT
-        ) == PackageManager.PERMISSION_GRANTED
+        return when {
+            isSdkVersionSupported() -> {
+                ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.USE_FINGERPRINT
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+            isBiometricPromptEnabled() -> {
+                ActivityCompat.checkSelfPermission(
+                        context,
+                        Manifest.permission.USE_BIOMETRIC
+                ) == PackageManager.PERMISSION_GRANTED
+            }
+            else -> {
+                false
+            }
+        }
     }
 }
